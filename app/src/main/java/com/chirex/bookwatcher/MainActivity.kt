1afae2b6e8ba1d96f803c52750a6b156e266d111
Mac <k2120853@kingston.ac.uk>
2025-01-24 20:38:34 +0000

    main menu buttons added

--- app/src/main/java/com/chirex/bookwatcher/MainActivity.kt
+++ app/src/main/java/com/chirex/bookwatcher/MainActivity.kt
@@ -3,27 +3,28 @@ package com.chirex.bookwatcher
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chirex.bookwatcher.ui.theme.BookWatcherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BookWatcherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MenuScreen()
                 }
             }
         }
@@ -31,17 +32,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MenuScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text("Create book entry")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text("View all book entries")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text("Edit book entry")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text("Delete book entry")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BookWatcherTheme {
-        Greeting("Android")
+        MenuScreen()
     }
 }
