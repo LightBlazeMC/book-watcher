d1d4dc640bacfd48d361ce4c3285da9b54e704c7
Mac <k2120853@kingston.ac.uk>
2025-01-31 14:26:17 +0000

    added navigation

--- app/src/main/java/com/chirex/bookwatcher/MainActivity.kt
+++ app/src/main/java/com/chirex/bookwatcher/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import java.util.regex.Pattern
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.chirex.bookwatcher.ui.theme.BookWatcherTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,25 +41,183 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MenuScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "MainMenu"
     ) {
+        composable("MainMenu") {
+            Menu(navController)
+        }
+        composable("addEntry") {
+            AddEntry(navController)
+        }
+        composable("viewEntries") {
+            ViewEntries(navController)
+        }
+        composable("editEntry") {
+            EditEntry(navController)
+        }
+        composable("deleteEntry") {
+            DeleteEntry(navController)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Menu(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Text("Book Watcher - Main Menu")
+                    }
+                }
+            )
+        }
+
+    ) { paddingValues ->
         Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = { /*TODO*/ }) {
-                Text("Create book entry")
+            Button(onClick = { navController.navigate("addEntry") }) {
+                Text("Add Entry")
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text("View all book entries")
+            Button(onClick = { navController.navigate("viewEntries") }) {
+                Text("View Entries")
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text("Edit book entry")
+            Button(onClick = { navController.navigate("editEntry") }) {
+                Text("Edit Entry")
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text("Delete book entry")
+            Button(onClick = { navController.navigate("deleteEntry") }) {
+                Text("Delete Entry")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddEntry(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Text("Book Watcher - Add Entry")
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Add Entry Screen")
+            Button(onClick = { navController.navigate("MainMenu") }) {
+                Text("Back to Menu")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ViewEntries(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Text("Book Watcher - View Entries")
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("View Entries Screen")
+            Button(onClick = { navController.navigate("MainMenu") }) {
+                Text("Back to Menu")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditEntry(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Text("Book Watcher - Edit Entry")
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Edit Entry Screen")
+            Button(onClick = { navController.navigate("MainMenu") }) {
+                Text("Back to Menu")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DeleteEntry(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Text("Book Watcher - Delete Entry")
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Delete Entry Screen")
+            Button(onClick = { navController.navigate("MainMenu") }) {
+                Text("Back to Menu")
             }
         }
     }
